d1e19b52c414677b8aa2713146fbc56437abfbe0
Olga G. <ofitasova@yahoo.com>
2025-06-22 18:52:34 -0500

    Delete TEMPrheumaview_ai_lite_debug.py

--- TEMPrheumaview_ai_lite_debug.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="RView-test", page_icon="🧪")
-
-st.title("👋 Привет из RView-test")
-st.write("Это базовое тестовое приложение работает корректно.")
-
-st.success("Streamlit полностью загружен.")
